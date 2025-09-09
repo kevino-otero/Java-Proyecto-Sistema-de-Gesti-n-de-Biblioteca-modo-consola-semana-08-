acd1ab5dd74a017c73ed3f0b5cf0d6a7f40fc715
DarrFun <darren.funes@gmail.com>
2025-09-08 21:07:31 -0600

    se modificó usuario por Darren

--- Usuario.java
+++ Usuario.java
@@ -1,29 +1,41 @@
-package ProgramacionII.RegistroII.Semana8;
-
 public class Usuario {
     private int id;
     private String nombre;
     private String identificacion;
 
     public Usuario(int id, String nombre, String identificacion) {
-        this.id = id; this.nombre = nombre; this.identificacion = identificacion;
+        this.id = id;
+        this.nombre = nombre;
+        this.identificacion = identificacion;
     }
 
-    public int getId() { return id; }
-    public String getNombre() { return nombre; }
-    public String getIdentificacion() { return identificacion; }
+    public int getId() {
+        return id;
+    }
 
-    public String toCsv() { return id + ";" + nombre + ";" + identificacion; }
-    public static Usuario fromCsv(String line) {
-        String[] x = line.split(";");
-        return new Usuario(Integer.parseInt(x[0]), x[1], x[2]);
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public String toCSV() {
+        return id + ";" + nombre + ";" + identificacion;
+    }
+
+    public static Usuario fromCSV(String line) {
+        String[] campos = line.split(";");
+        return new Usuario(
+            Integer.parseInt(campos[0]), 
+            campos[1], 
+            campos[2]
+        );
     }
 
     @Override
-    public String toString() { return id + " | " + nombre + " | " + identificacion; }
-
-    // Método agregado
-    public boolean esValido() {
-        return id > 0 && nombre != null && identificacion != null;
+    public String toString() {
+        return id + " | " + nombre + " | " + identificacion;
     }
 }
